f98ceb5a767edf3238e8b91f41404b14b66479c2
ccervantes1995 <christian.cervantes@media-meter.com>
2019-06-18 20:42:08 +0800

    updated and added

--- routes/filters.js
+++ routes/filters.js
@@ -15,10 +15,42 @@ router.get('/', function(req, res, next){
 })
 
 router.post('/store', function(req, res, next){
-
+    models.filters.storeFilter(req.body, function(err, result){
+        if(err) res.json(err);
+        else  res.json(result);
+    })
 })
 
 router.get('/add', function(req, res, next){
     res.render('filter/add', {title:'Add new Filters'})
 })
+
+router.get('/view/:id', function(req, res, next){
+    models.filters.findById(req.params.id).exec(function(err, result){
+        if(err) next(err);
+        else res.render('filter/view',  {title: req.params.id, data: result});
+    })
+})
+
+router.get('/edit/:id', function(req, res, next){
+    models.filters.findById(req.params.id).exec(function(err, result){
+        if(err) next(err);
+        else res.render('filter/edit',  {title: req.params.id, data: result});
+    })
+})
+
+router.post('/update/:id', function(req, res, next){
+    models.filters.findByIdAndUpdate(req.params.id, req.body).exec(function(err, result){
+        if(err) res.json(err);
+        else res.json(result);
+    })
+})
+
+router.get('/delete/:id', function(req, res, next){
+    models.filters.findByIdAndRemove(req.params.id).exec(function(err, result){
+        if(err) next(err);
+        else res.redirect('/filter');
+    })
+})
+
 module.exports = router;
